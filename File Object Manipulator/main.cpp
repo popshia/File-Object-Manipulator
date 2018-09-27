82c47387e0a9d08edac462360beca4dff550259e
popshia <B123292179@gmail.com>
2018-09-27 15:54:58 +0800

    Initial Commit

---
+++ File Object Manipulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  File Object Manipulator
+//
+//  Created by popshia on 2018/9/27.
+//  Copyright © 2018 popshia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
